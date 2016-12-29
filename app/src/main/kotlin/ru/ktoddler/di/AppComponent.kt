9681c79488e077d72c8d5e0747d7662602b1c90e
Artur Kharchenko <hyperax@gmail.com>
2016-12-29 13:25:29 +0300

    Add BaseActivity and BaseFragment

--- app/src/main/kotlin/ru/ktoddler/di/AppComponent.kt
+++ app/src/main/kotlin/ru/ktoddler/di/AppComponent.kt
@@ -5,6 +5,8 @@ import ru.ktoddler.KToddlerApp
 import ru.ktoddler.di.module.*
 import ru.ktoddler.di.scope.PerApplication
 import ru.ktoddler.view.MainActivity
+import ru.ktoddler.view.activity.BaseActivity
+import ru.ktoddler.view.fragment.BaseFragment
 
 @PerApplication
 @Component(modules = arrayOf(ApiModule::class, AppModule::class, DataModule::class,
@@ -13,6 +15,10 @@ interface AppComponent {
 
     fun inject(toddlerApp: KToddlerApp)
 
+    fun inject(baseActivity: BaseActivity)
+
     fun inject(mainActivity: MainActivity)
+
+    fun inject(baseFragment: BaseFragment)
 
 }
--- app/src/main/kotlin/ru/ktoddler/view/MainActivity.kt
+++ app/src/main/kotlin/ru/ktoddler/view/MainActivity.kt
@@ -1,25 +1,13 @@
 package ru.ktoddler.view
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import ru.ktoddler.KToddlerApp
-import ru.ktoddler.view.notification.KToddlerNotification
+import ru.ktoddler.view.activity.BaseActivity
 import ru.ktoddler.view.notification.Message
-import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var notifications: KToddlerNotification
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        (application as KToddlerApp).appComponent.inject(this)
-    }
+class MainActivity : BaseActivity() {
 
     override fun onResume() {
         super.onResume()
-        notifications.showToast(Message.alert("Toast message 1"))
-        notifications.showSnackbar(this, null, Message.alert("Snackbar message 2"))
+        showToast(Message.alert("Toast message 1"))
+        showSnackbar(Message.alert("Snackbar message 2"))
     }
 }
